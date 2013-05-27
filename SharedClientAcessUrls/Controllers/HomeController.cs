551f386d3545f04ab74df60cd48234c546b7a68c
devcurry <devcurry@a2zknowledgevisuals.com>
2013-05-28 01:24:53 +0530

    Code Commit

---
+++ SharedClientAcessUrls/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.WindowsAzure.StorageClient;
+using SharedClientAcessUrls.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SharedClientAcessUrls.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            IEnumerable<IListBlobItem> items = AzureBlobSA.GetContainerFiles("dnc-demo");
+            List<string> urls = new List<string>();
+            foreach (var item in items)
+            {
+                if (item is CloudBlockBlob)
+                {
+                    var blob = (CloudBlockBlob)item;
+                    urls.Add(blob.Name);
+                }
+            }
+            ViewBag.BlobFiles = urls;
+            ViewBag.Sas = AzureBlobSA.GetSasUrl("dnc-demo");
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetImage(string id)
+        {
+            string fileName = AzureBlobSA.DecodeFrom64(id);
+            return new RedirectResult(AzureBlobSA.GetSasBlobUrl("dnc-demo", fileName, Request.QueryString["sas"]));
+        }
+
+    }
+}
---
+++ SharedClientAcessUrls/Lib/AzureBlobSA.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SharedClientAcessUrls.Lib
+{
+    public class AzureBlobSA
+    {
+        public static IEnumerable<IListBlobItem> GetContainerFiles(string containerName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageAccountConnectionString"));
+            CloudBlobClient storageClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer storageContainer = storageClient.GetContainerReference(containerName);
+            return storageContainer.ListBlobs();
+
+        }
+        public static string GetSasUrl(string containerName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageAccountConnectionString"]);
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            container.CreateIfNotExist();
+
+            BlobContainerPermissions containerPermissions = new BlobContainerPermissions();
+
+            containerPermissions.SharedAccessPolicies.Add("twominutepolicy", new SharedAccessPolicy()
+            {
+                SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-1),
+                SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(2),
+                Permissions = SharedAccessPermissions.Write | SharedAccessPermissions.Read
+            });
+
+            containerPermissions.PublicAccess = BlobContainerPublicAccessType.Off;
+            container.SetPermissions(containerPermissions);
+            string sas = container.GetSharedAccessSignature(new SharedAccessPolicy(), "twominutepolicy");
+            return sas;
+        }
+
+        public static string GetSasBlobUrl(string containerName, string fileName, string sas)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageAccountConnectionString"]);
+
+            StorageCredentialsSharedAccessSignature sasCreds = new StorageCredentialsSharedAccessSignature(sas);
+            CloudBlobClient sasBlobClient = new CloudBlobClient(storageAccount.BlobEndpoint,
+            new StorageCredentialsSharedAccessSignature(sas));
+
+            CloudBlob blob = sasBlobClient.GetBlobReference(containerName + @"/" + fileName);
+            return blob.Uri.AbsoluteUri + sas;
+        }
+
+        static public string EncodeTo64(string toEncode)
+        {
+            byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+            string returnValue = System.Convert.ToBase64String(toEncodeAsBytes);
+            return returnValue;
+        }
+
+        static public string DecodeFrom64(string encodedData)
+        {
+            byte[] encodedDataAsBytes = System.Convert.FromBase64String(encodedData);
+            string returnValue = System.Text.ASCIIEncoding.ASCII.GetString(encodedDataAsBytes);
+            return returnValue;
+        }
+    }
+}
